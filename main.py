0033f3ff175353ce65b0981ff4eb8dbc4935f67f
stefanistrate28 <stefan.istrate@student.usv.ro>
2026-06-03 11:55:48 +0300

    modificare site

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from app.db.database import Base, engine
 from app.api.routes import warehouses, suppliers, products, inventory
-from fastapi.staticfiles import StaticFiles
 
-# cream aplicatia
-app = FastAPI(title="The Inventory Proiect")
+app = FastAPI(title="The Inventory Project")
+
+# permite requesturi din browser
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# cream tabelele in baza de date
 Base.metadata.create_all(bind=engine)
 
-
-
-# adaugam routes
 app.include_router(warehouses.router, prefix="/api/warehouses", tags=["Warehouses"])
 app.include_router(suppliers.router, prefix="/api/suppliers", tags=["Suppliers"])
 app.include_router(products.router, prefix="/api/warehouses/{warehouse_id}/products", tags=["Products"])
@@ -20,4 +25,4 @@ app.include_router(inventory.router, prefix="/api/warehouses/{warehouse_id}/inve
 
 @app.get("/")
 def root():
-    return {"message": "The Inventory !"}
+    return {"message": "buna, API-ul merge!"}
